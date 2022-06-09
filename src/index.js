f7e69cf4247f412addb2423f7512bbf40f8ba98f
vlovic <victorlovic@hotmail.com>
2022-06-10 01:28:43 +0200

    Add TODO list to index.js

--- src/index.js
+++ src/index.js
@@ -148,4 +148,18 @@ function Square(props) {
     }
     return null;
   }
+
+
+/*
+TODO
+
+- Display the location for each move in the format (col, row) in the move history list.
+- Bold the currently selected item in the move list.
+- Rewrite Board to use two loops to make the squares instead of hardcoding them.
+- Bonus: let the user select the size of the board, and the win condition (number of X/O in a row)
+- Add a toggle button that lets you sort the moves in either ascending or descending order.
+- When someone wins, highlight the three squares that caused the win.
+- When no one wins, display a message about the result being a draw.
+*/
+
   
